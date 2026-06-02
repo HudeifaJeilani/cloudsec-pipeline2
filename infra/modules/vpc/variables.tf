76061ee58dbed65d9ac5a66be6deb38f3bc2c75f
hjhudz <hudeifa.jeilani@hotmail.com>
2026-06-02 20:07:09 +0100

    create reusable VPC module with public and private subnets

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,31 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet1_cidr" {
+  type = string
+}
+
+variable "public_subnet2_cidr" {
+  type = string
+}
+
+variable "private_subnet1_cidr" {
+  type = string
+}
+
+variable "private_subnet2_cidr" {
+  type = string
+}
+
+variable "availability_zone_1" {
+  type = string
+}
+
+variable "availability_zone_2" {
+  type = string
+}
